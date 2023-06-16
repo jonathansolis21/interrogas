a7d52493406028f7bd0bd6582493930d299aee60
Jonathan Solis <johnny.r.solis@gmail.com>
2023-06-16 12:24:04 -0700

    Update ReferenceService.cs

--- .NET/ReferenceService.cs
+++ .NET/ReferenceService.cs
@@ -1,141 +1,123 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Interrogas.Data;
-using Interrogas.Data.Providers;
-using Interrogas.Models;
-using Interrogas.Models.Domain;
-using Interrogas.Models.Domain.LookUps;
-using Interrogas.Models.Requests.Reference;
-using Interrogas.Services.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Data.SqlClient;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Interrogas.Services
+public class ReferenceService : IReferenceService
 {
-    public class ReferenceService : IReferenceService
+    IDataProvider _data = null;
+    IInstitutionMapper _institutionMapper = null;
+    public ReferenceService(IDataProvider data, IInstitutionMapper institutionMapper)
     {
-        IDataProvider _data = null;
-        IInstitutionMapper _institutionMapper = null;
-        public ReferenceService(IDataProvider data, IInstitutionMapper institutionMapper)
+        _data = data;
+        _institutionMapper = institutionMapper; 
+    }
+    public int Add(ReferenceAddRequest model)
+    {
+        string procName = "[dbo].[References_Insert]";
+        int id = 0;
+        _data.ExecuteNonQuery(procName,
+        inputParamMapper: delegate (SqlParameterCollection refCol)
         {
-            _data = data;
-            _institutionMapper = institutionMapper; 
-        }
-        public int Add(ReferenceAddRequest model)
+            MapParams(model, refCol); 
+            SqlParameter refIdOut = new SqlParameter("@Id", SqlDbType.Int);
+            refIdOut.Direction = ParameterDirection.Output;
+            refCol.Add(refIdOut);
+        },
+        delegate (SqlParameterCollection returnRefCol)
         {
-            string procName = "[dbo].[References_Insert]";
-            int id = 0;
-            _data.ExecuteNonQuery(procName,
-            inputParamMapper: delegate (SqlParameterCollection refCol)
-            {
-                MapParams(model, refCol); 
-                SqlParameter refIdOut = new SqlParameter("@Id", SqlDbType.Int);
-                refIdOut.Direction = ParameterDirection.Output;
-                refCol.Add(refIdOut);
-            },
-            delegate (SqlParameterCollection returnRefCol)
-            {
-                object oId = returnRefCol["@Id"].Value;
-                Int32.TryParse(oId.ToString(), out id);
-            });
-            return id; 
-        }
-        public void Update(ReferenceUpdateRequest model)
+            object oId = returnRefCol["@Id"].Value;
+            Int32.TryParse(oId.ToString(), out id);
+        });
+        return id; 
+    }
+    public void Update(ReferenceUpdateRequest model)
+    {
+        string procName = "[dbo].[References_Update]";
+        _data.ExecuteNonQuery(procName,
+        inputParamMapper: delegate (SqlParameterCollection refCol)
         {
-            string procName = "[dbo].[References_Update]";
-            _data.ExecuteNonQuery(procName,
-            inputParamMapper: delegate (SqlParameterCollection refCol)
-            {
-                refCol.AddWithValue("@Id", model.Id);
-                MapParams(model, refCol); 
-            },
-            returnParameters: null); 
-        }
-        public Reference Get(int id)
+            refCol.AddWithValue("@Id", model.Id);
+            MapParams(model, refCol); 
+        },
+        returnParameters: null); 
+    }
+    public Reference Get(int id)
+    {
+        string procName = "[dbo].[References_SelectByIdV2]";
+        Reference reference = null;
+        _data.ExecuteCmd(procName,
+        delegate (SqlParameterCollection paramCol)
         {
-            string procName = "[dbo].[References_SelectByIdV2]";
-            Reference reference = null;
-            _data.ExecuteCmd(procName,
-            delegate (SqlParameterCollection paramCol)
-            {
-                paramCol.AddWithValue("@Id", id);
-            },
-            delegate (IDataReader reader, short set)
-            {
-                reference = MapReference(reader, out int startingIndex);
-            });
-            return reference; 
-        }
-        public Paged<Reference> Pagination(int pageIndex, int pageSize)
+            paramCol.AddWithValue("@Id", id);
+        },
+        delegate (IDataReader reader, short set)
         {
-            string procName = "[dbo].[References_SelectAllV2]";
-            Paged<Reference> pagedList = null;
-            List<Reference> list = null;
-            int totalCount = 0;
-            _data.ExecuteCmd(procName,
-            (param) =>
+            reference = MapReference(reader, out int startingIndex);
+        });
+        return reference; 
+    }
+    public Paged<Reference> Pagination(int pageIndex, int pageSize)
+    {
+        string procName = "[dbo].[References_SelectAllV2]";
+        Paged<Reference> pagedList = null;
+        List<Reference> list = null;
+        int totalCount = 0;
+        _data.ExecuteCmd(procName,
+        (param) =>
+        {
+            param.AddWithValue("@PageIndex", pageIndex);
+            param.AddWithValue("@PageSize", pageSize);
+        },
+        (reader, recordSetIndex) =>
+        {
+            Reference aReference = MapReference(reader, out int startingIndex);
+            totalCount = reader.GetInt32(startingIndex++);
+            if (list == null)
             {
-                param.AddWithValue("@PageIndex", pageIndex);
-                param.AddWithValue("@PageSize", pageSize);
-            },
-            (reader, recordSetIndex) =>
-            {
-                Reference aReference = MapReference(reader, out int startingIndex);
-                totalCount = reader.GetInt32(startingIndex++);
-                if (list == null)
-                {
-                    list = new List<Reference>();
-                }
-                list.Add(aReference);
-            });
-            if (list != null)
-            {
-                pagedList = new Paged<Reference>(list, pageIndex, pageSize, totalCount);
+                list = new List<Reference>();
             }
-            return pagedList;
-        }
-        public void StatusUpdate(int id)
+            list.Add(aReference);
+        });
+        if (list != null)
         {
-            string procName = "[dbo].[References_DeleteByStatusUpdate]";
-            _data.ExecuteNonQuery(procName,
-            inputParamMapper: delegate (SqlParameterCollection refCol)
-            {
-                refCol.AddWithValue("@Id", id);
-            },
-            returnParameters: null);
+            pagedList = new Paged<Reference>(list, pageIndex, pageSize, totalCount);
         }
-        public Reference MapReference(IDataReader reader, out int startingIndex)
+        return pagedList;
+    }
+    public void StatusUpdate(int id)
+    {
+        string procName = "[dbo].[References_DeleteByStatusUpdate]";
+        _data.ExecuteNonQuery(procName,
+        inputParamMapper: delegate (SqlParameterCollection refCol)
         {
-            Reference aReference = new Reference();
-            startingIndex = 0;
-            aReference.Id = reader.GetSafeInt32(startingIndex++);
-            aReference.Name = reader.GetSafeString(startingIndex++);
-            aReference.Date = reader.GetSafeDateTime(startingIndex++); 
-            aReference.FileId = reader.GetSafeInt32(startingIndex++);
-            aReference.FileName = reader.GetSafeString(startingIndex++);
-            aReference.ElectionYear = reader.GetSafeInt32(startingIndex++);
-            aReference.StatusId = reader.GetSafeInt32(startingIndex++);
-            aReference.Status = reader.GetSafeString(startingIndex++);
-            aReference.Institution = _institutionMapper.MapInstitution(reader, ref startingIndex);
-            return aReference;
-        }
-        private static void MapParams(ReferenceAddRequest model, SqlParameterCollection refCol)
-        {
-            refCol.AddWithValue("@Date", model.Date);
-            refCol.AddWithValue("@RefName", model.RefName);
-            refCol.AddWithValue("@FileId", model.FileId);
-            refCol.AddWithValue("@ElectionYear", model.ElectionYear);
-            refCol.AddWithValue("@StatusId", model.StatusId);
-            refCol.AddWithValue("@Name", model.Name);
-            refCol.AddWithValue("@SiteUrl", model.SiteUrl);
-            refCol.AddWithValue("@LogoUrl", model.LogoUrl);
-            refCol.AddWithValue("@StateId", model.StateId);
-            refCol.AddWithValue("@Code", model.Code);
-        }
+            refCol.AddWithValue("@Id", id);
+        },
+        returnParameters: null);
+    }
+    public Reference MapReference(IDataReader reader, out int startingIndex)
+    {
+        Reference aReference = new Reference();
+        startingIndex = 0;
+        aReference.Id = reader.GetSafeInt32(startingIndex++);
+        aReference.Name = reader.GetSafeString(startingIndex++);
+        aReference.Date = reader.GetSafeDateTime(startingIndex++); 
+        aReference.FileId = reader.GetSafeInt32(startingIndex++);
+        aReference.FileName = reader.GetSafeString(startingIndex++);
+        aReference.ElectionYear = reader.GetSafeInt32(startingIndex++);
+        aReference.StatusId = reader.GetSafeInt32(startingIndex++);
+        aReference.Status = reader.GetSafeString(startingIndex++);
+        aReference.Institution = _institutionMapper.MapInstitution(reader, ref startingIndex);
+        return aReference;
+    }
+    private static void MapParams(ReferenceAddRequest model, SqlParameterCollection refCol)
+    {
+        refCol.AddWithValue("@Date", model.Date);
+        refCol.AddWithValue("@RefName", model.RefName);
+        refCol.AddWithValue("@FileId", model.FileId);
+        refCol.AddWithValue("@ElectionYear", model.ElectionYear);
+        refCol.AddWithValue("@StatusId", model.StatusId);
+        refCol.AddWithValue("@Name", model.Name);
+        refCol.AddWithValue("@SiteUrl", model.SiteUrl);
+        refCol.AddWithValue("@LogoUrl", model.LogoUrl);
+        refCol.AddWithValue("@StateId", model.StateId);
+        refCol.AddWithValue("@Code", model.Code);
     }
 }
 
