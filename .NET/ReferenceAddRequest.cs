aeb03045f866f504cf632e305dede6c5912480fc
Jonathan Solis <johnny.r.solis@gmail.com>
2023-06-16 17:58:33 -0700

    Update ReferenceAddRequest.cs

--- .NET/ReferenceAddRequest.cs
+++ .NET/ReferenceAddRequest.cs
@@ -1,44 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Interrogas.Models.Requests.Reference
+public class ReferenceAddRequest
 {
-    public class ReferenceAddRequest
-    {
-		[Required]
-		public DateTime Date { get; set; }
-		[Required]
-		[StringLength (255, MinimumLength = 2)]
-		public string RefName { get; set; }
-		[Required]
-		[Range(1, Int32.MaxValue)]
-		public int FileId { get; set; }
-		[Required]
-		[Range(1992, 2060)]
-		public int ElectionYear { get; set; }
-		[Required]
-		[Range(1,Int32.MaxValue)]
-		public int StatusId { get; set; }
-		[Required]
-		[StringLength(255, MinimumLength = 2)]
-		public string Name { get; set; }
-		[Required]
-		[Url]
-		[StringLength(255, MinimumLength = 9)]
-		public string SiteUrl { get; set; }
-		[Required]
-		[Url]
-		[StringLength(255, MinimumLength = 9)]
-		public string LogoUrl { get; set; }
-		[Required]
-		[Range(1,Int32.MaxValue)]
-		public int StateId { get; set; }
-		[Required]
-		[StringLength(6, MinimumLength = 2)]
-		public string Code { get; set; }
-	}
+	[Required]
+	public DateTime Date { get; set; }
+	[Required]
+	[StringLength (255, MinimumLength = 2)]
+	public string RefName { get; set; }
+	[Required]
+	[Range(1, Int32.MaxValue)]
+	public int FileId { get; set; }
+	[Required]
+	[Range(1992, 2060)]
+	public int ElectionYear { get; set; }
+	[Required]
+	[Range(1,Int32.MaxValue)]
+	public int StatusId { get; set; }
+	[Required]
+	[StringLength(255, MinimumLength = 2)]
+	public string Name { get; set; }
+	[Required]
+	[Url]
+	[StringLength(255, MinimumLength = 9)]
+	public string SiteUrl { get; set; }
+	[Required]
+	[Url]
+	[StringLength(255, MinimumLength = 9)]
+	public string LogoUrl { get; set; }
+	[Required]
+	[Range(1,Int32.MaxValue)]
+	public int StateId { get; set; }
+	[Required]
+	[StringLength(6, MinimumLength = 2)]
+	public string Code { get; set; }
 }
+
