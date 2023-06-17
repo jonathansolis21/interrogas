024ae272ebe5c00f75b1edc49cf5706f4a394276
Jonathan Solis <johnny.r.solis@gmail.com>
2023-06-16 18:00:52 -0700

    Update ReferenceApiController.cs

--- .NET/ReferenceApiController.cs
+++ .NET/ReferenceApiController.cs
@@ -1,115 +1,114 @@
+[Route("api/references")]
+[ApiController]
+public class ReferenceApiController : BaseApiController
 {
-    [Route("api/references")]
-    [ApiController]
-    public class ReferenceApiController : BaseApiController
+    private IReferenceService _service = null;
+    public ReferenceApiController(IReferenceService service, ILogger<ReferenceApiController> logger) : base(logger)
     {
-        private IReferenceService _service = null;
-        public ReferenceApiController(IReferenceService service, ILogger<ReferenceApiController> logger) : base(logger)
+        _service = service;
+    }
+    [HttpPost]
+    public ActionResult<ItemResponse<int>> Create(ReferenceAddRequest model)
+    {
+        ObjectResult result = null;
+        try 
         {
-            _service = service;
+            int id = _service.Add(model);
+            ItemResponse<int> response = new ItemResponse<int>() { Item = id }; ;
+            result = Created201(response);
         }
-        [HttpPost]
-        public ActionResult<ItemResponse<int>> Create(ReferenceAddRequest model)
+        catch (Exception ex)
         {
-            ObjectResult result = null;
-            try 
-            {
-                int id = _service.Add(model);
-                ItemResponse<int> response = new ItemResponse<int>() { Item = id }; ;
-                result = Created201(response);
-            }
-            catch (Exception ex)
-            {
-                base.Logger.LogError(ex.ToString());
-                ErrorResponse response = new ErrorResponse(ex.Message);
-                result = StatusCode(500, response);
-            }
-            return result;
+            base.Logger.LogError(ex.ToString());
+            ErrorResponse response = new ErrorResponse(ex.Message);
+            result = StatusCode(500, response);
         }
-        [HttpPut("{id:int}")]
-        public ActionResult<SuccessResponse> Update(ReferenceUpdateRequest model)
+        return result;
+    }
+    [HttpPut("{id:int}")]
+    public ActionResult<SuccessResponse> Update(ReferenceUpdateRequest model)
+    {
+        int code = 200;
+        BaseResponse response = null;
+        try
         {
-            int code = 200;
-            BaseResponse response = null;
-            try
-            {
-                _service.Update(model);
-                response = new SuccessResponse();
-            }
-            catch (Exception ex)
-            {
-                code = 500;
-                response = new ErrorResponse(ex.Message);
-            }
-            return StatusCode(code, response);
+            _service.Update(model);
+            response = new SuccessResponse();
         }
-        [HttpGet("{id:int}")]
-        public ActionResult<ItemResponse<Reference>> Get(int id)
+        catch (Exception ex)
         {
-            int iCode = 200;
-            BaseResponse response = null;
-            try 
-            { 
-                Reference reference = _service.Get(id);
-                if (reference == null)
-                {
-                    iCode = 404;
-                    response = new ErrorResponse("Application resource not found."); 
-                }
-                else
-                {
-                    response = new ItemResponse<Reference> { Item = reference };
-                }
-            }
-            catch (Exception ex)
-            {
-                iCode = 500;
-                base.Logger.LogError(ex.ToString());
-                response = new ErrorResponse($"Generic Error: {ex.Message}"); 
-            }
-            return StatusCode(iCode, response); 
+            code = 500;
+            response = new ErrorResponse(ex.Message);
         }
-        [HttpGet("paginate")]
-        public ActionResult<ItemResponse<Paged<Reference>>> Pagination(int pageIndex, int pageSize)
+        return StatusCode(code, response);
+    }
+    [HttpGet("{id:int}")]
+    public ActionResult<ItemResponse<Reference>> Get(int id)
+    {
+        int iCode = 200;
+        BaseResponse response = null;
+        try 
+        { 
+            Reference reference = _service.Get(id);
+            if (reference == null)
+            {
+                iCode = 404;
+                response = new ErrorResponse("Application resource not found."); 
+            }
+            else
+            {
+                response = new ItemResponse<Reference> { Item = reference };
+            }
+        }
+        catch (Exception ex)
         {
-            ActionResult result = null;
-            try
-            {
-                Paged<Reference> paged = _service.Pagination(pageIndex, pageSize);
-                if (paged == null)
-                {
-                    result = NotFound404(new ErrorResponse("Pagination Resource Not Found."));
-                }
-                else
-                {
-                    ItemResponse<Paged<Reference>> response = new ItemResponse<Paged<Reference>>();
-                    response.Item = paged;
-                    result = Ok200(response);
-                }
-            }
-            catch (Exception ex)
-            {
-                Logger.LogError(ex.ToString());
-                result = StatusCode(500, new ErrorResponse(ex.Message.ToString()));
-            }
-            return result;
+            iCode = 500;
+            base.Logger.LogError(ex.ToString());
+            response = new ErrorResponse($"Generic Error: {ex.Message}"); 
         }
-        [HttpPut("status/{id:int}")] 
-        public ActionResult<SuccessResponse> DeleteReference(int id)
+        return StatusCode(iCode, response); 
+    }
+    [HttpGet("paginate")]
+    public ActionResult<ItemResponse<Paged<Reference>>> Pagination(int pageIndex, int pageSize)
+    {
+        ActionResult result = null;
+        try
         {
-            int code = 200;
-            BaseResponse response = null;
-            try
+            Paged<Reference> paged = _service.Pagination(pageIndex, pageSize);
+            if (paged == null)
             {
-                _service.StatusUpdate(id);
-                response = new SuccessResponse();
+                result = NotFound404(new ErrorResponse("Pagination Resource Not Found."));
             }
-            catch (Exception ex)
+            else
             {
-                code = 500;
-                response = new ErrorResponse(ex.Message);
+                ItemResponse<Paged<Reference>> response = new ItemResponse<Paged<Reference>>();
+                response.Item = paged;
+                result = Ok200(response);
             }
-            return StatusCode(code, response);
         }
+        catch (Exception ex)
+        {
+            Logger.LogError(ex.ToString());
+            result = StatusCode(500, new ErrorResponse(ex.Message.ToString()));
+        }
+        return result;
+    }
+    [HttpPut("status/{id:int}")] 
+    public ActionResult<SuccessResponse> DeleteReference(int id)
+    {
+        int code = 200;
+        BaseResponse response = null;
+        try
+        {
+            _service.StatusUpdate(id);
+            response = new SuccessResponse();
+        }
+        catch (Exception ex)
+        {
+            code = 500;
+            response = new ErrorResponse(ex.Message);
+        }
+        return StatusCode(code, response);
     }
 }
+
