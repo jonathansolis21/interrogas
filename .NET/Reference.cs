1cfe7aec9cc2d9185756d54ec44dca5b6ed5f6a5
Jonathan Solis <johnny.r.solis@gmail.com>
2023-06-16 17:58:07 -0700

    Update Reference.cs

--- .NET/Reference.cs
+++ .NET/Reference.cs
@@ -1,22 +1,12 @@
-﻿using Interrogas.Models.Domain.LookUps;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Interrogas.Models.Domain
+public class Reference
 {
-    public class Reference
-    {
-        public int Id { get; set; }
-        public Institution Institution { get; set; }
-        public DateTime Date { get; set; }
-        public string Name { get; set; }
-        public int FileId { get; set; }
-        public string FileName { get; set; }
-        public int ElectionYear { get; set; }
-        public int StatusId { get; set; }
-        public string Status { get; set; }
-    }
+    public int Id { get; set; }
+    public Institution Institution { get; set; }
+    public DateTime Date { get; set; }
+    public string Name { get; set; }
+    public int FileId { get; set; }
+    public string FileName { get; set; }
+    public int ElectionYear { get; set; }
+    public int StatusId { get; set; }
+    public string Status { get; set; }
 }
