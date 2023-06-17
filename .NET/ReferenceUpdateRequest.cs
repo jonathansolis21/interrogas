79e0f085e3430f667603900782647e9f910957bd
Jonathan Solis <johnny.r.solis@gmail.com>
2023-06-16 18:01:39 -0700

    Update ReferenceUpdateRequest.cs

--- .NET/ReferenceUpdateRequest.cs
+++ .NET/ReferenceUpdateRequest.cs
@@ -1,16 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Interrogas.Models.Requests.Reference
+public class ReferenceUpdateRequest : ReferenceAddRequest, IModelIdentifier
 {
-	public class ReferenceUpdateRequest : ReferenceAddRequest, IModelIdentifier
-    {
-		[Required]
-		[Range(1,Int32.MaxValue)]
-		public int Id { get; set; }
-	}
+	[Required]
+	[Range(1,Int32.MaxValue)]
+	public int Id { get; set; }
 }
