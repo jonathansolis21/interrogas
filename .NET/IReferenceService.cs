52be9c3195151364779a1c95569ca1d074141a8d
Jonathan Solis <johnny.r.solis@gmail.com>
2023-06-16 17:57:42 -0700

    Update IReferenceService.cs

--- .NET/IReferenceService.cs
+++ .NET/IReferenceService.cs
@@ -1,21 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Interrogas.Models;
-using Interrogas.Models.Domain;
-using Interrogas.Models.Requests.Reference;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Interrogas.Services.Interfaces
+public interface IReferenceService
 {
-    public interface IReferenceService
-    {
-        public int Add(ReferenceAddRequest model);
-        void Update(ReferenceUpdateRequest model);
-        public Reference Get(int id);
-        public Paged<Reference> Pagination(int pageIndex, int pageSize);
-        public void StatusUpdate(int id); 
-    }
+    public int Add(ReferenceAddRequest model);
+    void Update(ReferenceUpdateRequest model);
+    public Reference Get(int id);
+    public Paged<Reference> Pagination(int pageIndex, int pageSize);
+    public void StatusUpdate(int id); 
 }
+
